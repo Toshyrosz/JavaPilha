e45b639a391ae8078b3d1b9e83e4f21029b3b5f0
Toshyrosz <danielfs1989@gmail.com>
2023-01-05 14:14:14 -0300

    ArithmeticException e NullPointerException

--- javaPilha/src/javaPilha/Fluxo.java
+++ javaPilha/src/javaPilha/Fluxo.java
@@ -2,33 +2,42 @@ package javaPilha;
 
 public class Fluxo {
 
-    public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		metodo1();
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-       
-        try {	
-             metodo2();
-       }catch(ArithmeticException ex) {
-        	System.out.println("ArithmeticException");
-        }
-        System.out.println("Fim do metodo1");
-       
-    }
-    
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
 
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            	int a = i / 0;	
-            
-            
-        } 
-        System.out.println("Fim do metodo2");
-    }
+		try {	
+			metodo2();
+		}catch(ArithmeticException ex) {
+			String msg  = ex.getMessage();
+			System.out.println("ArithmeticException " + msg);
+
+			//Saida da chamada do metodo
+			//ex.printStackTrace();
+		} catch (NullPointerException ex) {
+			String msg  = ex.getMessage();
+			System.out.println("NullPointerException " + msg);
+			//ex.printStackTrace();
+		}
+		System.out.println("Fim do metodo1");
+
+	}
+
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			//int a = i / 0;	
+			Conta c = null;
+			c.deposita();
+
+		} 
+		System.out.println("Fim do metodo2");
+	}
 }
